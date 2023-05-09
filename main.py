569586f3a9d1700af680c9b25084cf3e6f3b9d34
Raven Cole <ravenscole@gmail.com>
2023-05-08 20:29:41 -0400

    Add audio files and make vlc run from playlist

--- main.py
+++ main.py
@@ -1,111 +1,134 @@
-from glob import glob
 import signal
-import sys
-import argparse
 import subprocess
-from time import sleep
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import sys
+
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 import vlc as VLC
 
-AUDIO_FILE = '/home/craven/test.mp3'
+from glob import glob
+from time import sleep
+
+AUDIO_DIRECTORY = '/home/craven/main/audio/*'
 PDF_DIRECTORY = '/home/craven/main/pdfs/*.pdf'
 INPUT_PIN = 10
-PLAYER = None
 LOOP_SLEEP = 0.2
+PDF_DIRECTORY = '/home/craven/main/pdfs/*.pdf'
+PLAYER = None
 
 def getInput():
-	return GPIO.input(INPUT_PIN)
+    return GPIO.input(INPUT_PIN)
+
 
 def getIsOffHook():
-	return getInput() == GPIO.LOW
+    return getInput() == GPIO.LOW
+
 
 def getIsOnHook():
-	return getInput() == GPIO.HIGH
+    return getInput() == GPIO.HIGH
+
+
+def initVLC():
+    print('init VLC')
+
+    global PLAYER
+
+    # creating a vlc instance
+    vlc = VLC.Instance()
+
+
+    tracks = glob(AUDIO_DIRECTORY)
+
+    medialist = vlc.media_list_new()
+
+    for t in tracks:
+        medialist.add_media(vlc.media_new(t))
+
+    PLAYER = vlc.media_list_player_new()
+
+    PLAYER.set_playback_mode(VLC.PlaybackMode.loop)
+
+    PLAYER.set_media_list(medialist)
+
+
+def initGPIO():
+    print('init GPIO')
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def initPhoneState():
+    print('init phone state')
+
+    while getIsOffHook():
+        print('Waiting for phone to be put on the hook.')
+        sleep(LOOP_SLEEP)
+
 
 def init():
-	print('Initializing...')
+    print('Initializing...')
 
-	print('Setting up VLC')
+    initVLC()
 
-	global PLAYER
+    initGPIO()
 
-	# Setup VLC
-	# ---------------------------
-	# creating a vlc instance
-	vlc = VLC.Instance('--input-repeat=999999')
+    initPhoneState()
 
-	# creating a media
-	media = vlc.media_new(AUDIO_FILE)
+    print('Initialization finished.')
 
-	# creating a media player
-	PLAYER = vlc.media_player_new()
-
-	# setting media to the player
-	PLAYER.set_media(media)
-
-	print('Setting up GPIO')
-
-	# Setup GPIO
-	# ---------------------------
-	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-	# Wait for phone to be hung up
-	# ---------------------------
-	while getIsOffHook():
-		print('Waiting for phone to be put on the hook.')
-		sleep(LOOP_SLEEP)
-
-	print('Initialization finished.')
 
 def loop():
-	print('Loop starting')
+    print('Loop starting')
 
-	playing = False
+    playing = False
 
-	pdfs = glob(PDF_DIRECTORY)
+    pdfs = glob(PDF_DIRECTORY)
 
-	i = 0;
+    i = 0;
 
-	while True:
-		if getIsOffHook() and playing == False:
-			print('Playing')
-			playing = True
-			PLAYER.play()
-		elif getIsOnHook() and playing == True:
-			print('Pausing')
-			playing = False
-			PLAYER.pause()
-			print('Printing')
-			pdf_file = pdfs[i]
-			subprocess.run(f'lp {pdf_file}', shell=True)
-			i += 1
-			if i > 3:
-				i = 0
+    while True:
+        if getIsOffHook() and playing == False:
+            print('Playing')
+            playing = True
+            PLAYER.play()
+        elif getIsOnHook() and playing == True:
+            print('Pausing')
+            playing = False
+            PLAYER.pause()
+            print('Printing')
+            pdf_file = pdfs[i]
+            subprocess.run(f'lp {pdf_file}', shell=True)
+            i += 1
+            if i > 3:
+                i = 0
 
-		sleep(LOOP_SLEEP)
+        sleep(LOOP_SLEEP)
+
 
 def cleanup():
-	print('Cleaning up...')
-	GPIO.cleanup()
+    print('Cleaning up...')
+    GPIO.cleanup()
+
 
 def signal_handler(sig, frame):
-	cleanup()
-	sys.exit(0)
+    cleanup()
+    sys.exit(0)
+
 
 def main():
-	# Set SIGINT intercept
-	signal.signal(signal.SIGINT, signal_handler)
+    # Set SIGINT intercept
+    signal.signal(signal.SIGINT, signal_handler)
 
-	# Setup
-	init()
+    # Setup
+    init()
 
-	# Run
-	loop()
+    # Run
+    loop()
 
-	# Bye
-	cleanup()
+    # Bye
+    cleanup()
+
 
 # Start
 main()
